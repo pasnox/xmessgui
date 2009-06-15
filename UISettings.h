73e6455a4b5b7126ee1dff33f39e4781e2bedc48
pasnox <pasnox@8a82a75c-5474-11de-971b-8d1939f35316>
2009-06-15 20:23:16 +0000

    Best roms settings in the UISettings dialog

--- UISettings.h
+++ UISettings.h
@@ -5,18 +5,20 @@
 #include "MachineModel.h"
 
 class Settings;
+class MachineFilterModel;
 
 class UISettings : public QDialog, public Ui::UISettings
 {
-		Q_OBJECT
+	Q_OBJECT
 
 public:
-	UISettings( Settings* settings, const MachineItemList& machines, QWidget* parent = 0 );
+	UISettings( Settings* settings, MachineModel* machineModel, QWidget* parent = 0 );
 	virtual ~UISettings();
 	
 protected:
 	Settings* mSettings;
-	MachineItemList mMachines;
+	MachineModel* mMachineModel;
+	MachineFilterModel* mMachineFilterModel;
 	QMap<QString, QString> mRomsPaths;
 	
 	void initSettings();
@@ -29,7 +31,7 @@ protected slots:
 	void on_tbBinaryFile_clicked();
 	void on_tbBiosPath_clicked();
 	void on_tbResourcesPath_clicked();
-	void on_cbRomsPath_currentIndexChanged( int index );
+	void on_tvRomsPath_activated( const QModelIndex &index );
 	void on_tbRomsPathSet_clicked();
 	void on_tbRomsPath_clicked();
 	void on_cbVideoMode_currentIndexChanged( const QString& text );
